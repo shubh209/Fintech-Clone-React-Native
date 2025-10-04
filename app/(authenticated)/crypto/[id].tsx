27571355ae34b35af56846aa5a23f518b03ae1a6
shubh209 <shubhkapadia2031@gmail.com>
2025-10-03 17:12:27 -0700

    3 Oct

--- app/(authenticated)/crypto/[id].tsx
+++ app/(authenticated)/crypto/[id].tsx
@@ -1,22 +1,63 @@
-import { SectionList, StyleSheet, Text, View } from 'react-native'
-import React from 'react'
+import { Image, ScrollView, SectionList, StyleSheet, Text, TouchableOpacity, View } from 'react-native'
+import React, { useState } from 'react'
 import { useHeaderHeight } from '@react-navigation/elements';
 import { Stack, useLocalSearchParams } from 'expo-router'
 import { defaultStyles } from '@/constants/Styles';
 import Colors from '@/constants/Colors';
+import { useQuery } from '@tanstack/react-query';
+import { Ionicons } from '@expo/vector-icons';
 
 const page = () => {
   const { id } = useLocalSearchParams();
   const headerHeight = useHeaderHeight();
+  const categories = ['Overview', 'News', 'Orders', 'Transactions'];
+  const [activeIndex, setActiveIndex] = useState(0);
 
+  const { data } = useQuery({
+    queryKey: ['info', id],
+    queryFn: async () => {
+      const info = await fetch(`/api/info?ids=${id}`).then((res) => res.json());
+      return info[+id];
+    },
+
+  });
+  
   return (
     <>
-      <Stack.Screen options={{ title: 'Bitcoin' }} />
+      <Stack.Screen options={{ title: data?.name }} />
       <SectionList
         style={{ paddingTop: headerHeight }}
         contentInsetAdjustmentBehavior='automatic'
         keyExtractor={(i) => i.title}
         sections={[{data: [{title:'Chart'}] }]}
+        renderSectionHeader={() => (
+          <ScrollView 
+            horizontal={true} 
+            showsHorizontalScrollIndicator={false}
+            contentContainerStyle={{
+              alignItems: 'center',
+              justifyContent: 'space-between',
+              width: '100%',
+              paddingHorizontal: 15,
+              paddingBottom: 6,
+              backgroundColor: Colors.background,
+              borderBottomColor: Colors.lightGray,
+              borderBottomWidth: StyleSheet.hairlineWidth,
+            }}
+          >
+            {categories.map((item, index)=> (
+              <TouchableOpacity
+                key={index}
+                style={activeIndex === index ? styles.categoriesBtnActive : styles.categoriesBtn}
+                onPress={() => setActiveIndex(index)}
+              >
+                <Text style={activeIndex === index ? styles.categoryTextActive : styles.categoryText}>
+                  {item}
+                </Text>
+              </TouchableOpacity>
+            ))}
+          </ScrollView>
+        )}
         ListHeaderComponent={() => (
           <>
             <View
@@ -27,7 +68,27 @@ const page = () => {
                 marginHorizontal: 16
               }}
             >
-              <Text style={styles.subtitle}>BTC</Text>
+              <Text style={styles.subtitle}>{data?.symbol}</Text>
+              <Image source={{uri: data?.logo}} style={{width:50, height: 50}} />
+            </View>
+
+            <View style={{ flexDirection: 'row', gap: 10, margin: 12 }}>
+              <TouchableOpacity
+                style={[
+                  defaultStyles.pillButtonSmall,
+                  { backgroundColor: Colors.primary, flexDirection: 'row', gap: 16 },
+                ]}>
+                <Ionicons name="add" size={24} color={'#fff'} />
+                <Text style={[defaultStyles.buttonText, { color: '#fff' }]}>Buy</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={[
+                  defaultStyles.pillButtonSmall,
+                  { backgroundColor: Colors.primaryMuted, flexDirection: 'row', gap: 16 },
+                ]}>
+                <Ionicons name="arrow-back" size={24} color={Colors.primary} />
+                <Text style={[defaultStyles.buttonText, { color: Colors.primary }]}>Receive</Text>
+              </TouchableOpacity>
             </View>
           </>
         )}
@@ -56,6 +117,29 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 20,
     color: Colors.gray,
+  },
+  categoryText: {
+    fontSize: 14,
+    color: Colors.gray,
+  },
+  categoryTextActive: {
+    fontSize: 14,
+    color: '#000',
+  },
+  categoriesBtn: {
+    padding: 10,
+    paddingHorizontal: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+    borderRadius: 20,
+  },
+  categoriesBtnActive: {
+    padding: 10,
+    paddingHorizontal: 14,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#fff',
+    borderRadius: 20,
   },
 });
 
